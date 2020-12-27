50c028c1fcfce216791b488f469e569d690d0b24
Barty <bartypitt@gmail.com>
2020-12-27 18:18:19 +0000

    changed spiff libary

--- Arduino/MemoryTestCode/MemoryTestCode.ino
+++ Arduino/MemoryTestCode/MemoryTestCode.ino
@@ -1,21 +1,28 @@
-#include <Ticker.h>
 
 #include "Arduino.h"
-#include <Adafruit_GPS.h>
-#include <Ticker.h>
-#include <logger_spiffs.h>
+#include "SPIFFS.h"
 
 
-LoggerSPIFFS myLog("/log/Data.log");
 bool senderHelp();
 int iterator = 1 ; 
 
 void setup() {
   Serial.begin(115200);
-  myLog.begin();
-  myLog.setFlusherCallback(senderHelp);
-  myLog.flush();
-  // put your setup code here, to run once:
+  
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+    File file = SPIFFS.open("/test.txt", FILE_WRITE);
+ if (!file) {
+    Serial.println("There was an error opening the file for writing");
+    return;
+  }
+  if (file.print("TEST")) {
+    Serial.println("File was written");
+  } else {
+    Serial.println("File write failed");
+  } 
 
 }
 
@@ -23,23 +30,6 @@ void loop() {
   delay(1000);
   String BigString = String( String(iterator , HEX) + "$");
   myLog.append(BigString.c_str(),false);
-  if (Serial){
-    myLog.flush();
-  }
-  // put your main code here, to run repeatedly:
+  
 
-}
-
-
-bool senderHelp(char* buffer, int n){
-  int index=0;
-  // Check if there is another string to print
-  while(index<n && strlen(&buffer[index])>0){
-    Serial.print("---");
-    int bytePrinted=Serial.print(&buffer[index]);
-    Serial.println("---");
-    // +1, the '\0' is processed
-    index += bytePrinted+1;
-  }
-  return true;
 }
